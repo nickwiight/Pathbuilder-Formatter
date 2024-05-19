6d5e4ed0e07abdac82e1cc13414d1dbb00fc3bf3
Nick White <nickwiight@gmail.com>
2024-05-19 00:25:15 -0700

    fix midsentance bolding

--- pathbuilder_formatter.rb
+++ pathbuilder_formatter.rb
@@ -4,11 +4,6 @@ BOLD_PHRASES = ['Critical Success', 'Critical Failure', 'Success', 'Failure',
                 'Prerequisites', 'Requirements', 'Frequency', 'Special',
                 'Access']
 NEWLINE_PHRASES = ['Critical Success', 'Special']
-STRING = "WALL MASTER 3 FEAT 13
-CONJURATION DUNGEON EARTH MANIPULATE
-Frequency once per day
-You call forth the notoriously sturdy walls from within your true dungeon body to subject your foes to the frustration of being lost or separated in a dungeon and forced to return to the entrance. This has the effects of the wall of stone spell.
-Special This feat gains the divine trait if you have the mausoleum heritage, arcane if you have another artificial dungeon heritage (such as labyrinth or tower) or primal if you have a natural one (such as an archipelago, caverns, leviathan, or tree)."
 
 file = File.open('input.txt')
 file_data = file.read
@@ -25,7 +20,7 @@ end
 def bold_format(string)
   copy = string.clone
   BOLD_PHRASES.each do |phrase|
-    regex = Regexp.new("(#{phrase})(?!<)")
+    regex = Regexp.new("(?<=\n)(#{phrase})(?!<)")
     copy.gsub!(regex, '<b>\0</b>')
   end
   copy
